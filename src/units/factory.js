f2fe9bb0cb5c2eea786128860683a50ce368cdbf
VsevolodX <vsevolodbiryukov@icloud.com>
2025-09-19 21:18:41 -0700

    update: unitf actore

--- src/units/factory.js
+++ src/units/factory.js
@@ -10,26 +10,44 @@ import { ProcessingUnit } from "./processing";
 import { SubworkflowUnit } from "./subworkflow";
 
 export class UnitFactory {
+    static AssertionUnit = AssertionUnit;
+
+    static AssignmentUnit = AssignmentUnit;
+
+    static BaseUnit = BaseUnit;
+
+    static ConditionUnit = ConditionUnit;
+
+    static ExecutionUnit = ExecutionUnit;
+
+    static IOUnit = IOUnit;
+
+    static MapUnit = MapUnit;
+
+    static ProcessingUnit = ProcessingUnit;
+
+    static SubworkflowUnit = SubworkflowUnit;
+
     static create(config) {
         switch (config.type) {
             case UNIT_TYPES.execution:
-                return new ExecutionUnit(config);
+                return new this.ExecutionUnit(config);
             case UNIT_TYPES.assignment:
-                return new AssignmentUnit(config);
+                return new this.AssignmentUnit(config);
             case UNIT_TYPES.condition:
-                return new ConditionUnit(config);
+                return new this.ConditionUnit(config);
             case UNIT_TYPES.io:
-                return new IOUnit(config);
+                return new this.IOUnit(config);
             case UNIT_TYPES.processing:
-                return new ProcessingUnit(config);
+                return new this.ProcessingUnit(config);
             case UNIT_TYPES.map:
-                return new MapUnit(config);
+                return new this.MapUnit(config);
             case UNIT_TYPES.subworkflow:
-                return new SubworkflowUnit(config);
+                return new this.SubworkflowUnit(config);
             case UNIT_TYPES.assertion:
-                return new AssertionUnit(config);
+                return new this.AssertionUnit(config);
             default:
-                return new BaseUnit(config);
+                return new this.BaseUnit(config);
         }
     }
 }
