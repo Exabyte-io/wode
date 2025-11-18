a82e3dcd91c5b6bc2037f1f858eafa6892fac364
VsevolodX <vsevolodbiryukov@icloud.com>
2025-11-17 20:55:11 -0800

    update: export from index

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,10 +3,88 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+Object.defineProperty(exports, "AssertionUnit", {
+  enumerable: true,
+  get: function () {
+    return _units.AssertionUnit;
+  }
+});
+Object.defineProperty(exports, "AssignmentUnit", {
+  enumerable: true,
+  get: function () {
+    return _units.AssignmentUnit;
+  }
+});
+Object.defineProperty(exports, "BaseUnit", {
+  enumerable: true,
+  get: function () {
+    return _units.BaseUnit;
+  }
+});
+Object.defineProperty(exports, "ConditionUnit", {
+  enumerable: true,
+  get: function () {
+    return _units.ConditionUnit;
+  }
+});
+Object.defineProperty(exports, "ExecutionUnit", {
+  enumerable: true,
+  get: function () {
+    return _units.ExecutionUnit;
+  }
+});
+Object.defineProperty(exports, "IOUnit", {
+  enumerable: true,
+  get: function () {
+    return _units.IOUnit;
+  }
+});
+Object.defineProperty(exports, "MapUnit", {
+  enumerable: true,
+  get: function () {
+    return _units.MapUnit;
+  }
+});
+Object.defineProperty(exports, "PointsPathFormDataProvider", {
+  enumerable: true,
+  get: function () {
+    return _PointsPathFormDataProvider.PointsPathFormDataProvider;
+  }
+});
+Object.defineProperty(exports, "ProcessingUnit", {
+  enumerable: true,
+  get: function () {
+    return _units.ProcessingUnit;
+  }
+});
+Object.defineProperty(exports, "ReduceUnit", {
+  enumerable: true,
+  get: function () {
+    return _units.ReduceUnit;
+  }
+});
 Object.defineProperty(exports, "Subworkflow", {
   enumerable: true,
   get: function () {
     return _subworkflows.Subworkflow;
+  }
+});
+Object.defineProperty(exports, "SubworkflowUnit", {
+  enumerable: true,
+  get: function () {
+    return _units.SubworkflowUnit;
+  }
+});
+Object.defineProperty(exports, "TAB_NAVIGATION_CONFIG", {
+  enumerable: true,
+  get: function () {
+    return _enums.TAB_NAVIGATION_CONFIG;
+  }
+});
+Object.defineProperty(exports, "UNIT_NAME_INVALID_CHARS", {
+  enumerable: true,
+  get: function () {
+    return _enums.UNIT_NAME_INVALID_CHARS;
   }
 });
 Object.defineProperty(exports, "UNIT_STATUSES", {
@@ -25,6 +103,12 @@ Object.defineProperty(exports, "UnitFactory", {
   enumerable: true,
   get: function () {
     return _factory.UnitFactory;
+  }
+});
+Object.defineProperty(exports, "WORKFLOW_STATUSES", {
+  enumerable: true,
+  get: function () {
+    return _enums.WORKFLOW_STATUSES;
   }
 });
 Object.defineProperty(exports, "Workflow", {
@@ -63,8 +147,31 @@ Object.defineProperty(exports, "createWorkflows", {
     return _workflows.createWorkflows;
   }
 });
+Object.defineProperty(exports, "defaultMapConfig", {
+  enumerable: true,
+  get: function () {
+    return _map.defaultMapConfig;
+  }
+});
+Object.defineProperty(exports, "globalSettings", {
+  enumerable: true,
+  get: function () {
+    return _settings.globalSettings;
+  }
+});
+Object.defineProperty(exports, "wodeProviders", {
+  enumerable: true,
+  get: function () {
+    return _providers.wodeProviders;
+  }
+});
+var _providers = require("./context/providers");
+var _PointsPathFormDataProvider = require("./context/providers/PointsPathFormDataProvider");
+var _settings = require("./context/providers/settings");
 var _enums = require("./enums");
 var _subworkflows = require("./subworkflows");
+var _units = require("./units");
 var _builders = require("./units/builders");
 var _factory = require("./units/factory");
+var _map = require("./units/map");
 var _workflows = require("./workflows");
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,29 @@
-import { UNIT_STATUSES, UNIT_TYPES } from "./enums";
+import { wodeProviders } from "./context/providers";
+import { PointsPathFormDataProvider } from "./context/providers/PointsPathFormDataProvider";
+import { globalSettings } from "./context/providers/settings";
+import {
+    TAB_NAVIGATION_CONFIG,
+    UNIT_NAME_INVALID_CHARS,
+    UNIT_STATUSES,
+    UNIT_TYPES,
+    WORKFLOW_STATUSES,
+} from "./enums";
 import { createSubworkflowByName, Subworkflow } from "./subworkflows";
+import {
+    AssertionUnit,
+    AssignmentUnit,
+    BaseUnit,
+    ConditionUnit,
+    ExecutionUnit,
+    IOUnit,
+    MapUnit,
+    ProcessingUnit,
+    ReduceUnit,
+    SubworkflowUnit,
+} from "./units";
 import { builders } from "./units/builders";
 import { UnitFactory } from "./units/factory";
+import { defaultMapConfig } from "./units/map";
 import { createWorkflow, createWorkflowConfigs, createWorkflows, Workflow } from "./workflows";
 
 export {
@@ -15,4 +37,21 @@ export {
     builders,
     UNIT_TYPES,
     UNIT_STATUSES,
+    TAB_NAVIGATION_CONFIG,
+    UNIT_NAME_INVALID_CHARS,
+    WORKFLOW_STATUSES,
+    BaseUnit,
+    ExecutionUnit,
+    AssertionUnit,
+    AssignmentUnit,
+    ConditionUnit,
+    IOUnit,
+    MapUnit,
+    ProcessingUnit,
+    ReduceUnit,
+    SubworkflowUnit,
+    defaultMapConfig,
+    wodeProviders,
+    PointsPathFormDataProvider,
+    globalSettings,
 };
