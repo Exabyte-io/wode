bd495bc2e42442daf6df737983cf77c8538d171c
VsevolodX <vsevolodbiryukov@icloud.com>
2025-12-09 12:16:21 -0800

    update: add wf logic

--- src/py/mat3ra/wode/utils.py
+++ src/py/mat3ra/wode/utils.py
@@ -1,4 +1,32 @@
 from mat3ra.utils.uuid import get_uuid
+import re
+from typing import Any, List, Optional
+
 
 def generate_uuid() -> str:
     return get_uuid()
+
+
+def find_by_name_or_regex(items: List[Any], name: Optional[str] = None, name_regex: Optional[str] = None) -> Optional[Any]:
+    """
+    Find an item in a list by exact name match or regex pattern.
+
+    Args:
+        items: List of objects to search through
+        name: Exact name to match
+        name_regex: Regex pattern to match against names
+
+    Returns:
+        First matching item or None
+    """
+    if name:
+        for item in items:
+            if item.name == name:
+                return item
+    elif name_regex:
+        pattern = re.compile(name_regex)
+        for item in items:
+            if pattern.search(item.name):
+                return item
+    return None
+
--- src/py/mat3ra/wode/workflows/workflow.py
+++ src/py/mat3ra/wode/workflows/workflow.py
@@ -2,11 +2,12 @@ from typing import List, Optional
 
 from mat3ra.code.entity import InMemoryEntitySnakeCase
 from mat3ra.esse.models.workflow import WorkflowSchema
+from mat3ra.standata.subworkflows import SubworkflowStandata
 from pydantic import Field
 
 from ..subworkflows import Subworkflow
 from ..units import Unit
-from ..utils import generate_uuid
+from ..utils import find_by_instance_or_id, find_by_name_or_regex, generate_uuid
 
 
 class Workflow(WorkflowSchema, InMemoryEntitySnakeCase):
@@ -24,6 +25,14 @@ class Workflow(WorkflowSchema, InMemoryEntitySnakeCase):
     subworkflows: List[Subworkflow] = Field(default_factory=list)
     units: List[Unit] = Field(default_factory=list)
     isMultiMaterial: bool = Field(default=False)
+
+    @property
+    def application(self):
+        if not self.subworkflows or len(self.subworkflows) == 0:
+            return None
+
+        first_subworkflow = self.subworkflows[0]
+        return first_subworkflow.application if first_subworkflow.application else None
 
     @classmethod
     def from_subworkflow(cls, subworkflow: Subworkflow) -> "Workflow":
@@ -69,19 +78,34 @@ class Workflow(WorkflowSchema, InMemoryEntitySnakeCase):
     def set_units(self, units: List[Unit]):
         raise NotImplementedError
 
-    # TODO: implement for MIN notebook
     def get_unit_by_name(self, name: Optional[str] = None, name_regex: Optional[str] = None) -> Optional[Unit]:
-        raise NotImplementedError
+        return find_by_name_or_regex(self.units, name=name, name_regex=name_regex)
 
-    # TODO: implement for MIN notebook
     def set_unit(
-            self, unit: Optional[Unit] = None, unit_flowchart_id: Optional[str] = None, new_unit: Optional[Unit] = None
+            self,
+            new_unit: Unit,
+            unit: Optional[Unit] = None,
+            unit_flowchart_id: Optional[str] = None,
     ) -> bool:
-        raise NotImplementedError
+        target_unit = find_by_instance_or_id(
+            self.units,
+            instance=unit,
+            instance_id=unit_flowchart_id,
+            id_attribute="flowchartId"
+        )
+
+        try:
+            unit_index = self.units.index(target_unit)
+        except ValueError:
+            return False
+
+        self.units[unit_index] = new_unit
+        return True
 
     def set_context_to_unit(self, unit_name: Optional[str] = None, unit_name_regex: Optional[str] = None,
                             new_context: Optional[Dict[str, Any]] = None):
-        raise NotImplementedError
+        target_unit = self.get_unit_by_name(name=unit_name, name_regex=unit_name_regex)
+        target_unit.context = new_context
 
     def add_unit(self, unit: Unit, head: bool = False, index: int = -1):
         raise NotImplementedError
@@ -92,10 +116,46 @@ class Workflow(WorkflowSchema, InMemoryEntitySnakeCase):
     def add_unit_type(self, unit_type: str, head: bool = False, index: int = -1):
         raise NotImplementedError
 
-    def toggle_relaxation(self):
-        raise NotImplementedError
+    @property
+    def relaxation_subworkflow(self) -> Optional[Subworkflow]:
+        application_name = self.application.name
+        subworkflow_standata = SubworkflowStandata()
+        relaxation_data = subworkflow_standata.get_relaxation_subworkflow_by_application(application_name)
 
-    # TODO: implement for MIN notebook
+        if not relaxation_data:
+            return None
+
+        return Subworkflow(**relaxation_data)
+
+    def is_relaxation_subworkflow(self, subworkflow: Subworkflow) -> bool:
+        relaxation = self.relaxation_subworkflow
+        if not relaxation:
+            return False
+
+        return subworkflow.name == relaxation.name
+
+    @property
+    def has_relaxation(self) -> bool:
+        for subworkflow in self.subworkflows:
+            if self.is_relaxation_subworkflow(subworkflow):
+                return True
+        return False
+
+    def toggle_relaxation(self):
+        if self.has_relaxation:
+            relaxation_subworkflow = None
+            for subworkflow in self.subworkflows:
+                if self.is_relaxation_subworkflow(subworkflow):
+                    relaxation_subworkflow = subworkflow
+                    break
+
+            if relaxation_subworkflow:
+                self.remove_subworkflow_by_id(relaxation_subworkflow.id)
+        else:
+            relaxation = self.relaxation_subworkflow
+            if relaxation:
+                self.add_subworkflow(relaxation, head=True)
+
     def add_relaxation(self):
-        # self.toggle_relaxation()
-        raise NotImplementedError
+        if not self.has_relaxation:
+            self.toggle_relaxation()
