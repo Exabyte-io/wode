4c3db9ae45253371cc6148e80238ae77e7c5f7da
VsevolodX <vsevolodbiryukov@icloud.com>
2025-12-09 19:11:30 -0800

    chore: lint fix

--- src/py/mat3ra/wode/context/providers/ml_train_test_split_context_provider.py
+++ src/py/mat3ra/wode/context/providers/ml_train_test_split_context_provider.py
@@ -1,7 +1,5 @@
 # TODO: We need applicationContextMixin equivalent in Python
 
-from typing import Any, Dict
-
 from mat3ra.ade.context.context_provider import ContextProvider
 from mat3ra.esse.models.context_providers_directory.ml_train_test_split_context_provider import (
     MLTrainTestSplitContextProviderSchema,
