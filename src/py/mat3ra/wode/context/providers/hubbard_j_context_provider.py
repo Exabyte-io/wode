62d8595867c56657d0db55ac228d0ab40bd2b0bc
VsevolodX <vsevolodbiryukov@icloud.com>
2025-12-11 17:30:32 -0800

    chore: remove

--- src/py/mat3ra/wode/context/providers/hubbard_j_context_provider.py
+++
@@ -1,7 +0,0 @@
-from .hubbard_u_context_provider import HubbardUContextProvider
-
-
-class HubbardJContextProvider(HubbardUContextProvider):
-    """
-    Context provider for Hubbard J settings.
-    """
