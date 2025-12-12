34c92394cec62ddda47be25084579d369880de79
VsevolodX <vsevolodbiryukov@icloud.com>
2025-12-11 16:33:21 -0800

    update: adjust tests

--- tests/py/units/test_io_unit.py
+++ tests/py/units/test_io_unit.py
@@ -4,4 +4,4 @@ from mat3ra.wode import IOUnit
 def test_default_values():
     unit = IOUnit(type="io", name="test", subtype="input", source="api")
     assert unit.type == "io"
-    assert unit.subtype == "input"
+    assert unit.subtype.value == "input"
