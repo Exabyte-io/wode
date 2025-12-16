a4ed82059f46a89ab41ccd42782ac4db3e141a47
VsevolodX <vsevolodbiryukov@icloud.com>
2025-12-16 12:57:17 -0800

    chore: lint fix 2

--- src/py/mat3ra/wode/units/subworkflow.py
+++ src/py/mat3ra/wode/units/subworkflow.py
@@ -1,10 +1,10 @@
-from typing import Optional
+from typing import Optional, get_args
 
 from mat3ra.esse.models.workflow.unit.subworkflow import SubworkflowUnitSchema
+from pydantic import Field
 
-from ..utils import field_from_schema
 from .unit import Unit
 
 
 class SubworkflowUnit(Unit, SubworkflowUnitSchema):
-    type: Optional[str] = field_from_schema(SubworkflowUnitSchema, "type")
+    type: Optional[str] = Field(default=get_args(SubworkflowUnitSchema.model_fields["type"].annotation)[0])
