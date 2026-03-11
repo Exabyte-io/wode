e896ad562c7e006a0d50c707ec8d7c4882ff24e6
VsevolodX <vsevolodbiryukov@icloud.com>
2026-03-11 14:04:42 -0700

    update: add path provider

--- src/py/mat3ra/wode/context/providers/__init__.py
+++ src/py/mat3ra/wode/context/providers/__init__.py
@@ -1,3 +1,4 @@
 from .points_grid_data_provider import PointsGridDataProvider
+from .points_path_data_provider import PointsPathDataProvider
 
-__all__ = ["PointsGridDataProvider"]
+__all__ = ["PointsGridDataProvider", "PointsPathDataProvider"]
